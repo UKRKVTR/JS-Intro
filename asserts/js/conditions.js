0dda5ec50c4f7041ee88144b526cc1bc1a3e5e7d
Danylo <warfais6@gmail.com>
2023-08-17 21:30:38 +0300

    Done Work

--- asserts/js/conditions.js
+++ asserts/js/conditions.js
@@ -1,31 +1,46 @@
-let n = 0
-let rez = 0
+let n = 0;
+let rez = 0;
 
-while (n <=10){
-++n
-rez = n * 3
-console.log(n, '*3 =', res)
+while (n <= 10) {
+  ++n;
+  rez = n * 3;
+  console.log(n, "*3 =", rez);
 }
 
-for (let i = 1; i <= 10; i++) {
-  console.log('Таблиця множення для числа', i,':')
-
-  for (let j = 1; j <= 10; j++) {
-      const res = i * j;
-      console.log(i, '*', j, '=', res);
+let i = 0;
+let res = 0;
+while (i < 10) {
+  ++i;
+  console.log("Таблиця множення для числа", i, ":");
+  let j = 0;
+  while (j < 10) {
+    ++j;
+    res = i * j;
+    console.log(i, "*", j, "=", res);
   }
-  
 }
 
-let a = +prompt('First number'); 
-let b = +prompt('Second number');  
-let x = 0; 
-while(a < b){ 
-  ++a 
-  if(a % 2 == 0 && a > 0 && b > 0){ 
-    console.log(a) 
-      x+=a 
-  } 
- 
-}  
-  console.log(x)
+let a = +prompt("First number");
+let b = +prompt("Second number");
+let x = 0;
+while (a < b) {
+  ++a;
+  if (a % 2 == 0 && a > 0 && b > 0) {
+    console.log(a);
+    x += a;
+  }
+}
+console.log(x);
+
+let k = 1
+let p = 0;
+let s = 0;
+let ser= 0;
+while(a != 0){
+    k = +prompt('')
+    p+=k;
+    s++;
+}
+console.log('Кількість: ',s-1)
+console.log('Сумма: ',p)
+console.log('Середнє: ',ser = p/s)
